50775adc955a8376ff6ec7f3a83f6d36599ea280
Vishal Gupta <vishalg8897@gmail.com>
2017-10-24 14:43:38 +0530

    Can now send folders as zips

--- kdeconnect-ext.py
+++ kdeconnect-ext.py
@@ -2,6 +2,7 @@ import importlib
 from subprocess import check_output, call
 from gi.repository import GObject
 from kdeconnect import send_files, get_available_devices
+import zipfile
 
 TARGET = "%%TARGET%%".title()
 Nautilus = importlib.import_module("gi.repository.{}".format(TARGET))
@@ -11,14 +12,27 @@ class KDEConnectExtension(GObject.GObject, Nautilus.MenuProvider):
     def __init__(self):
         pass
 
-    def menu_activate_cb(self, menu, files, device_id, device_name):
-        send_files(files, device_id, device_name)
-
     def zipdir(self, path, ziph):
         # ziph is zipfile handle
+        import os
         for root, dirs, files in os.walk(path):
             for file in files:
                 ziph.write(os.path.join(root, file))
+
+    def menu_activate_cb(self, menu, files, device_id, device_name, folder_list):
+        import zipfile
+        import os
+        for folder_name in folder_list:
+            zip_name = folder_name + '.zip'
+            zipf = zipfile.ZipFile(zip_name[7:], 'w', zipfile.ZIP_DEFLATED)
+            self.zipdir(folder_name + '/', zipf)
+            zipf.close()
+            zipfile = Nautilus.FileInfo.create_for_uri(zip_name)
+            files.append(zipfile)
+
+        send_files(files, device_id, device_name)
+        # for folder_name in folder_list:
+        #     os.remove(folder_name[7:] + '.zip')
 
     def get_file_items(self, window, files):
         try:
@@ -27,21 +41,18 @@ class KDEConnectExtension(GObject.GObject, Nautilus.MenuProvider):
             raise Exception("Failed to get available devices")
 
         files_new = []
+        folder_list = []
         for i in range(len(files)):
             if (files[i].is_directory() == False):
                 files_new.append(files[i])
             else:
-                folder_name = files[i].get_uri()
-                zip_name = folder_name + '.zip'
-                zipf = zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED)
-                self.zipdir(folder_name + '/', zipf)
-                zipf.close()
-                zipfile_object = Nautilus.FileInfo.create_for_uri(zip_name)
-                files_new.append(zipfile_object)
+                folder_list.append(files[i].get_uri())
 
         files = files_new
+        print files
+        print folder_list
 
-        if (len(files_new) < 1):
+        if (len(files_new) + len(folder_list) < 1):
             return []
 
         items = []
@@ -51,7 +62,7 @@ class KDEConnectExtension(GObject.GObject, Nautilus.MenuProvider):
                 label="Send to %s" % device["name"]
             )
             item.connect('activate', self.menu_activate_cb,
-                         files, device["id"], device["name"])
+                         files, device["id"], device["name"], folder_list)
             items.append(item)
 
         return items
